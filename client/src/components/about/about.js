78293244bcb7aef34584e153a14b760add5e13f9
Alexander Schyr <vr0922@partner.kit.edu>
2024-03-07 17:33:06 +0100

    component folder structure, about page

---
+++ client/src/components/about/about.js
@@ -0,0 +1,26 @@
+const Info = () => {
+    return (
+      <div className="info">
+        <h2>About Us</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+          nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+          eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+          numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+          doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+          Recusandae, ad!
+        </p>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora
+          nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum
+          eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores
+          numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto
+          doloremque. Corporis in et placeat unde sapiente perspiciatis minus!
+          Recusandae, ad!
+        </p>
+      </div>
+    );
+  };
+  export default Info;  
