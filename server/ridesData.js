3f1f6ff6aa4a2f9df04f3792b32370c96c8c750d
Kshitij Deshmukh <142789304+stickrundude@users.noreply.github.com>
2024-03-13 19:13:23 +0100

    Update ridesData.js
    updated car images

--- server/ridesData.js
+++ server/ridesData.js
@@ -1,12 +1,12 @@
-export default [
+export const rides = [
   {
     "person": "John",
     "time": "09:30",
     "date": "2024-03-01", 
     "model": "Tesla Model S",
     "distance": "150 km",
-    "origin": "City A",
-    "destination": "City B",
+    "origin": "Paris",
+    "destination": "Berlin",
     "imagePath": "https://hips.hearstapps.com/hmg-prod/images/2024-tesla-model-s-107-6572200e43fa1.jpg?crop=0.473xw:0.355xh;0.254xw,0.341xh&resize=1200:*"
   },
   {
@@ -15,8 +15,9 @@ export default [
     "date": "2024-03-02",
     "model": "Nissan Leaf",
     "distance": "120 km",
-    "origin": "City C",
-    "destination": "City D"
+    "origin": "Rome",
+    "destination": "Madrid",
+    "imagePath": "https://images.prismic.io/carwow/a9b25a99-74ae-4ce8-bf98-d20fd9b2b927_Front+%C2%BE+moving.jpg"
   },
   {
     "person": "John",
@@ -24,8 +25,9 @@ export default [
     "date": "2024-03-03", 
     "model": "Chevrolet Bolt",
     "distance": "200 km",
-    "origin": "City E",
-    "destination": "City F"
+    "origin": "London",
+    "destination": "Amsterdam",
+    "imagePath": "https://media.ed.edmunds-media.com/chevrolet/bolt-ev/2023/oem/2023_chevrolet_bolt-ev_4dr-hatchback_2lt_fq_oem_1_1280.jpg"
   },
   {
     "person": "Emily",
@@ -33,8 +35,9 @@ export default [
     "date": "2024-03-04", 
     "model": "BMW i3",
     "distance": "180 km",
-    "origin": "City G",
-    "destination": "City H"
+    "origin": "Barcelona",
+    "destination": "Lisbon",
+    "imagePath": "https://upload.wikimedia.org/wikipedia/commons/d/d9/BMW_i3_%E2%80%93_Frontansicht%2C_5._Oktober_2014%2C_D%C3%BCsseldorf.jpg"
   },
   {
     "person": "Emily",
@@ -42,8 +45,9 @@ export default [
     "date": "2024-03-05",
     "model": "Volkswagen ID.4",
     "distance": "170 km",
-    "origin": "City I",
-    "destination": "City J"
+    "origin": "Vienna",
+    "destination": "Prague",
+    "imagePath": "https://i.auto-bild.de/ir_img/3/3/3/3/9/7/1/DB2023AU00848_large-0ea7d717c64c72eb.jpg"
   },
   {
     "person": "John",
@@ -51,8 +55,9 @@ export default [
     "date": "2024-03-06",
     "model": "Hyundai Kona Electric",
     "distance": "190 km",
-    "origin": "City K",
-    "destination": "City L"
+    "origin": "Athens",
+    "destination": "Budapest",
+    "imagePath": "https://ev-database.org/img/auto/Hyundai_Kona_Electric_2024/Hyundai_Kona_Electric_2024-01@2x.jpg"
   },
   {
     "person": "Emily",
@@ -60,8 +65,9 @@ export default [
     "date": "2024-03-07", 
     "model": "Kia Soul EV",
     "distance": "160 km",
-    "origin": "City M",
-    "destination": "City N"
+    "origin": "Berlin",
+    "destination": "Prague",
+    "imagePath": "https://www.topgear.com/sites/default/files/2023/06/Large-22108-SoulEVExplore.jpg"
   },
   {
     "person": "Emily",
@@ -69,8 +75,9 @@ export default [
     "date": "2024-03-08", 
     "model": "Jaguar I-PACE",
     "distance": "220 km",
-    "origin": "City O",
-    "destination": "City P"
+    "origin": "Amsterdam",
+    "destination": "Brussels",
+    "imagePath": "https://ecomento.de/wp-content/uploads/2023/08/Jaguar-I-Pace.jpg"
   },
   {
     "person": "Jack",
@@ -78,8 +85,9 @@ export default [
     "date": "2024-03-09", 
     "model": "Audi e-tron",
     "distance": "210 km",
-    "origin": "City Q",
-    "destination": "City R"
+    "origin": "Vienna",
+    "destination": "Budapest",
+    "imagePath": "https://www.motortrend.com/uploads/2022/07/2023-Audi-E-tron-sportback-6693-front-three-quarter.jpg"
   },
   {
     "person": "Jack",
@@ -87,8 +95,9 @@ export default [
     "date": "2024-03-10", 
     "model": "Ford Mustang Mach-E",
     "distance": "240 km",
-    "origin": "City S",
-    "destination": "City T"
+    "origin": "Paris",
+    "destination": "Rome",
+    "imagePath": "https://www.gpas-cache.ford.com/guid/f257c286-09b6-309f-8a3f-16dbb5fa81dc.jpg"
   },
   {
     "person": "Jack",
@@ -96,8 +105,9 @@ export default [
     "date": "2024-03-11", 
     "model": "Porsche Taycan",
     "distance": "230 km",
-    "origin": "City U",
-    "destination": "City V"
+    "origin": "Madrid",
+    "destination": "Barcelona",
+    "imagePath": "https://images-porsche.imgix.net/-/media/54642AAF378E454FAE3048EAEC3EB6F3_47F70AEBD9FD44ABB73D9B4387D12A88_taycan-turbo-s-front-and-side?w=1299&q=85&auto=format"
   },
   {
     "person": "Jack",
@@ -105,8 +115,9 @@ export default [
     "date": "2024-03-12", 
     "model": "Rivian R1T",
     "distance": "250 km",
-    "origin": "City W",
-    "destination": "City X"
+    "origin": "London",
+    "destination": "Berlin",
+    "imagePath": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/2022_Rivian_R1T_%28in_Glacier_White%29%2C_front_6.21.22.jpg/1200px-2022_Rivian_R1T_%28in_Glacier_White%29%2C_front_6.21.22.jpg"
   },
   {
     "person": "Jack",
@@ -114,8 +125,9 @@ export default [
     "date": "2024-03-13", 
     "model": "Mercedes-Benz EQC",
     "distance": "260 km",
-    "origin": "City Y",
-    "destination": "City Z"
+    "origin": "Berlin",
+    "destination": "Prague",
+    "imagePath": "https://cdn.motor1.com/images/mgl/XWRYE/s1/2020-mercedes-benz-eqc.jpg"
   },
   {
     "person": "Noah",
@@ -123,8 +135,9 @@ export default [
     "date": "2024-03-14", 
     "model": "Lucid Air",
     "distance": "270 km",
-    "origin": "City AA",
-    "destination": "City BB"
+    "origin": "Rome",
+    "destination": "Paris",
+    "imagePath": "https://ev-database.org/img/auto/Lucid_Air/Lucid_Air-01@2x.jpg"
   },
   {
     "person": "Noah",
@@ -132,8 +145,9 @@ export default [
     "date": "2024-03-15", 
     "model": "Rimac C_Two",
     "distance": "280 km",
-    "origin": "City CC",
-    "destination": "City DD"
+    "origin": "Madrid",
+    "destination": "Lisbon",
+    "imagePath": "https://web-cdn.rimac-automobili.com/wp-content/uploads/2020/08/01075917/c_two_studio_front34_01.jpg"
   },
   {
     "person": "Noah",
@@ -141,8 +155,9 @@ export default [
     "date": "2024-03-16", 
     "model": "Faraday Future FF 91",
     "distance": "290 km",
-    "origin": "City EE",
-    "destination": "City FF"
+    "origin": "Athens",
+    "destination": "Berlin",
+    "imagePath": "https://cdn.motor1.com/images/mgl/YAWe0q/s3/faraday-future-ff-91.jpg"
   },
   {
     "person": "Noah",
@@ -150,8 +165,9 @@ export default [
     "date": "2024-03-17", 
     "model": "Byton M-Byte",
     "distance": "300 km",
-    "origin": "City GG",
-    "destination": "City HH"
+    "origin": "Vienna",
+    "destination": "Prague",
+    "imagePath": "https://hips.hearstapps.com/hmg-prod/images/2021-byton-m-byte-mmp-1-1-1578422067.jpg"
   },
   {
     "person": "Noah",
@@ -159,8 +175,9 @@ export default [
     "date": "2024-03-18", 
     "model": "Canoo Lifestyle Vehicle",
     "distance": "310 km",
-    "origin": "City II",
-    "destination": "City JJ"
+    "origin": "Amsterdam",
+    "destination": "Brussels",
+    "imagePath": "https://www.motortrend.com/uploads/2022/11/Canoo-LDV-28.jpg"
   },
   {
     "person": "Noah",
@@ -168,8 +185,9 @@ export default [
     "date": "2024-03-19", 
     "model": "Fisker Ocean",
     "distance": "320 km",
-    "origin": "City KK",
-    "destination": "City LL"
+    "origin": "Paris",
+    "destination": "Barcelona",
+    "imagePath": "https://upload.wikimedia.org/wikipedia/commons/9/9b/Fisker_Ocean_Motorworld_Munich_1X7A0079.jpg"
   },
   {
     "person": "Noah",
@@ -177,7 +195,9 @@ export default [
     "date": "2024-03-20", 
     "model": "Bollinger B1",
     "distance": "330 km",
-    "origin": "City MM",
-    "destination": "City NN"
+    "origin": "London",
+    "destination": "Madrid",
+    "imagePath": "https://upload.wikimedia.org/wikipedia/commons/5/53/Bollinger_B1%2C_front_left_%28Greenwich_Concours%29.jpg"
   }
 ];
+  
