a94ed93a6622e530ad31b16465bb347330792c70
Shane <sotiris@sexycoders.org>
2024-03-14 15:30:21 +0100

    trying to fix coverage

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,7 +6,7 @@ import PastRidesDetails from "./PastRides/PastRidesDetails"
 // Import necessary components and hooks from react-router-dom
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 import { CNavbar, CContainer, CNavbarBrand, CNavbarToggler, CCollapse, CNavItem, CNavbarNav} from '@coreui/react';
-import Profile from './Profile';
+import Profile from './Profile/Profile';
 import userProfile from './john_doe.jpeg';
 
 import ReserveARide from './ReserveARide/reservearide';
