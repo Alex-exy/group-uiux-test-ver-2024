285c81927675c327d95c78301a69503981df62f0
Alexander Schyr <vr0922@partner.kit.edu>
2024-03-14 17:38:21 +0100

    fixes

--- client/src/components/ReserveARide/ListRides.test.js
+++ client/src/components/ReserveARide/ListRides.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { act } from 'react-dom/test-utils';
 import ListRides from './ListRides';
 import ReserveARidePage from './ReserveARidePage';
+
+const mockData = [
+  { id: 1, type: 'Car', distance: '10km', battery: '70%', info: 'Additional info' },
+  { id: 2, type: 'Bike', distance: '5km', battery: '90%', info: 'Additional info' }
+];
 
 describe('ReserveARide Component', () => {
   let wrapper = null;
@@ -22,8 +28,6 @@ describe('ReserveARide Component', () => {
   });
 
   // Test 02: Clicking a row
-  
-  /*
   it('navigates to reserve-a-ride when the first row is clicked', () => {
     const handleRowClick = jest.fn();
     const firstRow = wrapper.find('CTableRow').first();
@@ -38,10 +42,8 @@ describe('ReserveARide Component', () => {
         <ReserveARidePage />
       </Router>
     );
-
-    expect(reserveARideWrapper.exists()).toBe(true);
     */
-
-  //});
+    //expect(reserveARideWrapper.exists()).toBe(true);
+  });
   
 });
