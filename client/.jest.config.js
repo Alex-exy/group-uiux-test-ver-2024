10406914d2ca2d893344da384b004abb2961e2a7
Shane <sotiris@sexycoders.org>
2024-03-15 04:35:33 +0100

    stopped trying leave jest as is

--- client/.jest.config.js
+++ client/.jest.config.js
@@ -1,10 +1,37 @@
 module.exports = {
-  transform: {
-    "^.+\\.(js|jsx)$": "babel-jest"
-  },
+  testEnvironment: 'jsdom',
+  // Collect coverage reports
+  collectCoverage: true,
+  
+  // Where to output coverage reports
+  coverageDirectory: "coverage",
+
+  // Mock CSS and LESS imports to prevent syntax errors
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "identity-obj-proxy"
+    "\\.(css|less)$": "<rootDir>/__mocks__/fileMock.js"
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"] // Update this line with the correct path
+
+  // Transform settings to use babel-jest for JS/JSX files
+  transform: {
+    "^.+\\.[t|j]sx?$": "babel-jest"
+  },
+
+  // Specify transformation ignore patterns except for specific modules like axios
+  transformIgnorePatterns: [
+    "node_modules/(?!axios).+\\.js$"
+  ],
+
+  // Setup Enzyme (if you're using it) or other testing utilities
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
+
+  // Coverage thresholds (optional, customize as needed)
+  coverageThreshold: {
+    global: {
+      branches: 50,
+      functions: 50,
+      lines: 50,
+      statements: -10
+    }
+  }
 };
 
